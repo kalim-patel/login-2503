af0517156d8e4baf16e5a62b2eb135f45dbba9bd
kalim-patel <kalimp555@gmail.com>
2026-09-22 05:12:39 +0530

    Update git clone URL for login app

--- proj_modules/login-script.sh
+++ proj_modules/login-script.sh
@@ -10,5 +10,5 @@ sudo apt install -y nginx
 echo "Remove Sample Pages"
 sudo rm -r /var/www/html/
 echo "Clone Login App"
-sudo git clone https://github.com/ravi2krishna/login-2503.git /var/www/html
+sudo git clone https://github.com/kalim-patel/login-2503.git /var/www/html
 echo "Web App Deployment Completed"
