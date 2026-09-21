dbc2bc82092686be2061562f88a54d0110f6ecee
kalim-patel <kalimp555@gmail.com>
2026-09-22 05:14:56 +0530

    Change availability zone for login VPC module

--- proj_modules/login_app.tf
+++ proj_modules/login_app.tf
@@ -2,7 +2,7 @@ module "login_vpc" {
     source = "./modules/vpc"
     vpc_cidr = "10.0.0.0/16"
     public_subnet_cidr = "10.0.0.0/24"
-    availability_zone = "us-east-2a"
+    availability_zone = "eu-north-1a"
     vpc_name          = "login"
 }
 
